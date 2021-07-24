107b740d1778e889afc9c97206713c0cb03e8d11
adbarc92 <adbarclay92@gmail.com>
2021-07-24 11:49:10 -0600

    Adds some example API responses

---
+++ client/data/exampleProductIdRes.js
@@ -0,0 +1,22 @@
+module.exports = {
+  id: 18078,
+  campus: 'hr-bld',
+  name: 'Camo Onesie',
+  slogan: 'Blend in to your crowd',
+  description:
+    'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
+  category: 'Jackets',
+  default_price: '140.00',
+  created_at: '2021-02-23T05:08:00.350Z',
+  updated_at: '2021-02-23T05:08:00.350Z',
+  features: [
+    {
+      feature: 'Fabric',
+      value: 'Canvas',
+    },
+    {
+      feature: 'Buttons',
+      value: 'Brass',
+    },
+  ],
+};
