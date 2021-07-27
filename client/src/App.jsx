3f1cfe8a32ff00773530103c612734a8d59d8071
Olivia Hall <ohall1223@gmail.com>
2021-07-26 20:22:13 -0600

    Monday destroy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Reviews from './components/R&R/Reviews.jsx';
+import Questions from './components/Q&A/questions.jsx';
 
 const App = () => {
   return (
     <div>
       <div>Hello World</div>
       <Reviews />
+      <Questions/>
     </div>
   );
 };
---
+++ client/src/components/Q&A/questions.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Questions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+            This is the Questions and Answers section 
+      </div>
+    );
+  }
+}
+
+export default Questions;
