aabe19f5e3fcd04261935cf1201713a6da58ea2c
jordan-trimarchi <ajordant47@gmail.com>
2021-07-28 12:16:00 -0600

    bruh

--- client/src/components/overview/ProductOverview.jsx
+++ client/src/components/overview/ProductOverview.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 
-import ProductDetails from '/ProductDetails.jsx'; // TodoQ: why not check for JSX?
+import ProductDetails from './ProductDetails.jsx'; // TodoQ: why not check for JSX?
 
 import mockProductData from 'data/exampleProductIdRes';
 
