fa8bf87bf7ceb8e930d87e3270c2cf5543ab20ff
jordan-trimarchi <ajordant47@gmail.com>
2021-07-28 12:05:51 -0600

    added './'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Reviews from './components/R&R/Reviews.jsx';
 import Questions from './components/Q&A/questions.jsx';
-import ProductOverview from 'components/overview/ProductOverview.jsx';
+import ProductOverview from './components/overview/ProductOverview.jsx';
 import ProductProvider from './components/ProductProvider.jsx';
 
 const port = 4000;
