5f56a026528572fe5cdd5aad0cf8612fb5d46100
adbarc92 <adbarclay92@gmail.com>
2021-07-29 16:05:06 -0600

    Clean-up on Aisle Overview

--- client/src/components/overview/ProductOverview.jsx
+++ client/src/components/overview/ProductOverview.jsx
@@ -6,40 +6,18 @@ import ProductDetails from './ProductDetails'; // TodoQ: why not check for JSX?
 import { ProductContext } from '../../context';
 
 const ProductOverview = () => {
-  // const [productData, setProductData] = useState(null);
-  // const [loaded, setLoaded] = useState(false);
-  // const [error, setError] = useState(false);
   const { reviewsMeta, product, productStyles } = useContext(ProductContext);
 
-  // useEffect(() => {
-  //   try {
-  //     setProductData(mockProductData);
-  //     setLoaded(true);
-  //   } catch (e) {
-  //     setError(e);
-  //   }
-  // }, []);
-
-  // if (error) {
-  //   return <div>An error has occurred</div>;
-  // }
-
   return (
-    <div>
-      {/* {loaded ? ( */}
-      <>
-        <div>{JSON.stringify(reviewsMeta.ratings)}</div>
-        <ProductDetails
-          productCategory={product.category || ''}
-          productName={product.name || ''}
-          productPrice={product.default_price || ''}
-        />
-        <div>{JSON.stringify(productStyles)}</div>
-      </>
-      {/* ) : (
-        <div>Loading...</div>
-      )} */}
-    </div>
+    <>
+      <div>{JSON.stringify(reviewsMeta.ratings)}</div>
+      <ProductDetails
+        productCategory={product.category || ''}
+        productName={product.name || ''}
+        productPrice={product.default_price || ''}
+      />
+      <div>{JSON.stringify(productStyles)}</div>
+    </>
   );
 };
 
