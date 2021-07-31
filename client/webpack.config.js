c61ce9503354278932f7c44a6122ebf5ed15504a
adbarc92 <adbarclay92@gmail.com>
2021-07-31 08:45:37 -0600

    Builds locally

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -62,6 +62,7 @@ const config = {
       data: path.join(__dirname, '..', '..', '..', 'server', 'data'),
       modules: path.join(__dirname, '../../node_modules/'),
       server: path.join(__dirname, '..', '..', '..', 'server'),
+      react: path.resolve(__dirname, '..', 'node_modules', 'react'),
     },
     extensions: ['.js', '.jsx'], // * This cancels requirement for file extensions
   },
@@ -74,11 +75,7 @@ module.exports = () => {
     config.plugins.push(new MiniCssExtractPlugin());
   } else {
     config.mode = 'development';
-    config.resolve.alias.react = path.resolve(
-      __dirname,
-      'node_modules',
-      'react',
-    );
+    // config.resolve.alias.react =
   }
   return config;
 };
