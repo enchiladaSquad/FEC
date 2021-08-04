a2d5f94bece3777f0832d3898808df2c7e26339c
jordan-trimarchi <ajordant47@gmail.com>
2021-08-04 15:04:36 -0600

    move product test

--- client/src/components/R&R/R&R.test.jsx
+++ client/src/components/R&R/R&R.test.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 
-describe('ProductDetails', () => {
-	test.only('the simplest test', () => {
-    const aDiv = render(<div />);
+// describe('ProductDetails', () => {
+// 	test.only('the simplest test', () => {
+//     const aDiv = render(<div />);
 
-    expect(aDiv).toBeTruthy();
-  });
-});
+//     expect(aDiv).toBeTruthy();
+//   });
+// });
