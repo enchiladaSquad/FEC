b95fbeef21be818aa27c89dc5bf152ad2a53de9e
adbarc92 <adbarclay92@gmail.com>
2021-09-09 14:10:09 -0600

    Splits webpack

---
+++ client/webpack.dev.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = {
+  entry: './src/index.jsx',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/i,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      src: path.join(__dirname, 'src'),
+      components: path.join(__dirname, 'src', 'components'),
+      data: path.join(__dirname, '..', '..', '..', 'server', 'data'),
+      modules: path.join(__dirname, '..', '..', 'node_modules'),
+      server: path.join(__dirname, '..', '..', '..', 'server'),
+      react: path.resolve(__dirname, '..', 'node_modules', 'react'),
+    },
+    extensions: ['.js', '.jsx'], // * This cancels requirement for file extensions
+  },
+};
